daa426b307ddb131cf17dd2e8fa90854c7d21d47
vinaymarni <vinaymarni@gmail.com>
2021-11-21 11:59:27 +0530

    SPA

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+import Z_FooterApp from './components/Z_FooterApp';
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+      <hr />
+      <Z_FooterApp />
+    </div>
+  );
+};
